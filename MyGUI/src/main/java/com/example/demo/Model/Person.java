ac574c24aa21c1924978bfe98ef729e93d2bca84
Bjergu <63778239+Bjergu@users.noreply.github.com>
2020-10-15 20:50:56 +0200

    Add files via upload

---
+++ MyGUI/src/main/java/com/example/demo/Model/Person.java
@@ -0,0 +1,31 @@
+package com.example.demo.Model;
+
+public class Person {
+    private String first_name;
+    private String last_name;
+    private String species;
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+}
